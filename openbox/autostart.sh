6d9c31c42082fb512ae6a3150e9e7d7676e00cf5
Pavel Shevchuk <stlwrt@gmail.com>
2008-12-16 14:13:30 +0200

    Set wallpaper depending on hostname

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,13 +1,24 @@
-# This shell script is run before Openbox launches.
-# Environment variables set here are passed to the Openbox session.
-
-# D-bus
+# dbus
 if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
        eval `dbus-launch --sh-syntax --exit-with-session`
 fi
 
-docker &
+# set wallpaper
+case "$HOSTNAME" in
+  delta.scwlab.com)
+    feh --bg-tile ~/Images/wpaps/background-2.jpg &
+    ;;
+  work.scwlab.com)
+    feh --bg-center ~/Pictures/wpaps/fire-final.jpg &
+    ;;
+  *)
+    xsetroot -solid "#303030"
+    ;;
+esac
 
-setxkbmap -model geniuscomfy2 -layout "lv,ru(winkeys)" -option "grp:caps_toggle" &
-feh --bg-tile ~/Images/wpaps/background-2.jpg &
+# utils
+docker &
 urxvtd &
+
+# keyboard layout
+setxkbmap -model geniuscomfy2 -layout "lv,ru(winkeys)" -option "grp:caps_toggle" &
