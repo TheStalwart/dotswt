e48d77a2a01c41e0556e60f25f7ec24404c178db
Pavel Shevchuk <stlwrt@gmail.com>
2023-11-21 21:39:58 +0200

    Introduced "stree" PowerShell alias for parity with mac version

--- WindowsPowerShell/Profile.ps1
+++ WindowsPowerShell/Profile.ps1
@@ -5,3 +5,9 @@ function l {
 function la {
     eza -aG
 }
+
+# https://stackoverflow.com/questions/19663202/how-do-you-open-sourcetree-from-the-command-line
+function stree {
+    # https://pureinfotech.com/list-environment-variables-windows-10/
+    & $Env:LocalAppData\SourceTree\SourceTree.exe -f (Get-Location)
+}
