73c02063a9c2eeff021dbf5ac8a44c1967396574
Pavel Shevchuk <stlwrt@gmail.com>
2025-07-03 17:15:48 +0300

    Introduced dotswt_steamnointro PowerShell function to disable Steam Big Picture startup animation

--- WindowsPowerShell/Profile.ps1
+++ WindowsPowerShell/Profile.ps1
@@ -35,6 +35,13 @@ function gdiff {
     & "C:\Program Files\git\usr\bin\diff" "--color=auto" @Args
 }
 
+# install a hack to disable Steam Big Picture startup animation
+# https://www.reddit.com/r/Steam/comments/sf1jnr/disable_steam_big_pictures_annoying_intro_partial/
+function dotswt_steamnointro {
+    New-Item -ItemType Directory -Path 'C:\Program Files (x86)\Steam\config\uioverrides\movies' -ErrorAction SilentlyContinue
+    Invoke-WebRequest -Uri 'https://github.com/freeload101/SCRIPTS/raw/master/MISC/blank.webm' -OutFile 'C:\Program Files (x86)\Steam\config\uioverrides\movies\bigpicture_startup.webm'
+}
+
 # Make Ctrl+D exit shell, among other familiar keybinds
 # https://stackoverflow.com/a/63022342
 Set-PSReadlineOption -EditMode Emacs
