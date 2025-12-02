2298c1a1e786eedf909c2c51084810a0319ac020
Pavel Shevchuk <stlwrt@gmail.com>
2025-12-02 13:51:11 +0200

    I never use proxies

--- nixos/gpd-pocket.nix
+++ nixos/gpd-pocket.nix
@@ -34,10 +34,6 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
