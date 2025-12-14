d51bd0bbdf50e3bd480e3e933ba6f293d84d132f
Pavel Shevchuk <stlwrt@gmail.com>
2025-12-15 00:46:00 +0200

    Introduced macon.nix

---
+++ nixos/macon.nix
@@ -0,0 +1,24 @@
+# NixOS Incus image is broken on TrueNAS out of box.
+# To fix it:
+# - Stop the container
+# - Open System -> Shell in TrueNAS Web Interface
+#   - `sudo -s`
+#   - `incus config set CONTAINERNAME security.nesting=true`
+# - Start the container
+# - Edit /etc/nix/nix.conf, set `nix.settings.sandbox = false;`
+# - `nix-channel --update`
+# - `nixos-rebuild switch --upgrade`
+# - Stop the container
+# - Open System -> Shell
+#   - `sudo -s`
+#   - `incus config unset CONTAINERNAME security.nesting`
+# - Start the container
+
+# TrueNAS Incus dashboard opens "Container Shell" in a broken state.
+# Execute `su -` to evaluate all configuration files properly.
+
+{ config, pkgs, ... }:
+
+{
+  services.httpd.enable = true;
+}
