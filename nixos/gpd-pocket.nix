7c1a4bba235c1039b8532a058d8001f1755262f5
Pavel Shevchuk <stlwrt@gmail.com>
2026-01-21 02:24:02 +0200

    Adjusted and moved xserver.xkb settings, turns out they're ignored by Wayland compositors

--- nixos/gpd-pocket.nix
+++ nixos/gpd-pocket.nix
@@ -38,12 +38,6 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
--- nixos/shared/i18n.nix
+++ nixos/shared/i18n.nix
@@ -8,6 +8,12 @@
     LC_TIME = "lv_LV.UTF-8"; # use 24-hour clock format
   };
 
+  # Configure keyboard layouts
+  services.xserver.xkb = {
+    layout = "lv,ru";
+    options = "grp:alt_shift_toggle";
+  };
+
   # https://search.nixos.org/options?query=timezone
   time.timeZone = "Europe/Riga";
 }
