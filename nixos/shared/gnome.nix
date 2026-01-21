acd9006d253c487cb0b5733969472bbe39e4de82
Pavel Shevchuk <stlwrt@gmail.com>
2026-01-21 17:23:13 +0200

    Added Caffeine extension to GNOME nix config

--- nixos/shared/gnome.nix
+++ nixos/shared/gnome.nix
@@ -3,8 +3,14 @@
 # Copy or symlink this file to /etc/nixos/configuration.nix
 # and run `nixos-rebuild switch --upgrade-all` to sync system state
 
+{ pkgs, ... }:
+
 {
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.caffeine
+  ];
 }
