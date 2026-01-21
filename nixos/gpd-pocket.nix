ecdbfb8ddd609797c3036dfef65b30d731d6dd08
Pavel Shevchuk <stlwrt@gmail.com>
2026-01-21 13:33:06 +0200

    Extracted pipewire config into a separate nix file

--- nixos/gpd-pocket.nix
+++ nixos/gpd-pocket.nix
@@ -20,6 +20,7 @@
     ./shared/kde.nix
     ./shared/gui-apps.nix
     ./shared/i18n.nix
+    ./shared/pipewire.nix
   ];
 
   # Bootloader.
@@ -37,22 +38,6 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
