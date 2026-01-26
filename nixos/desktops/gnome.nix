a270e67e9b622180d634e3c0094855463c99a643
Pavel Shevchuk <stlwrt@gmail.com>
2026-01-27 01:29:27 +0200

    Updated comments in gnome.nix to keep it as a reminder why i'm choosing other desktop environments

--- nixos/desktops/gnome.nix
+++ nixos/desktops/gnome.nix
@@ -1,7 +1,13 @@
 # Define the GNOME Desktop Environment configuration
 
-# Copy or symlink this file to /etc/nixos/configuration.nix
+# Include this file in /etc/nixos/configuration.nix imports section
 # and run `nixos-rebuild switch --upgrade-all` to sync system state
+
+# GNOME is highly opinionated, dare i say it... xenophobic.
+# It only makes sense if you're using mostly GNOME apps 
+# and like the iPadOS-like window management.
+# GNOME Calendar fails to load with my iCloud account synced,
+# and that leaves me with no reasons to stay on GNOME.
 
 { pkgs, ... }:
 
