1ac03e7590c250abe23cec0a0ead406e79e196a5
Pavel Shevchuk <stlwrt@gmail.com>
2026-01-27 13:49:31 +0200

    ly doesn't support auto login, use COSMIC Greeter for VMs

--- nixos/shared/insecure-vm.nix
+++ nixos/shared/insecure-vm.nix
@@ -1,9 +1,14 @@
 # Disables password prompts
 # to streamline UX when used in VM
 
+{ lib, ... }:
+
 {
   security.sudo.wheelNeedsPassword = false;
 
+  # ly doesn't support auto login, use COSMIC Greeter instead
+  services.displayManager.ly.enable = lib.mkForce false;
+  services.displayManager.cosmic-greeter.enable = lib.mkForce true;
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "stalwart";
 }
