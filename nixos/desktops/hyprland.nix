adc8443212afb7e93112ba8720aabce9d2d454d7
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-02 18:50:49 +0200

    Added noctalia-shell, nwg-displays and quintom-cursor-theme to hyprland.nix

--- nixos/desktops/hyprland.nix
+++ nixos/desktops/hyprland.nix
@@ -3,6 +3,8 @@
 # Include this file in /etc/nixos/configuration.nix imports section
 # and run `nixos-rebuild switch --upgrade-all` to sync system state
 
+{ pkgs, ... }:
+
 {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
@@ -12,4 +14,17 @@
     withUWSM = true; # recommended for most users
     xwayland.enable = true; # required for Steam
   };
+
+  # I don't need scaling,
+  # so don't bother with hyprcursor or SVG cursor themes for now.
+  # nordzy-cursor-theme nixpkg comes with hyprcursor version
+  # https://wiki.hypr.land/Hypr-Ecosystem/hyprcursor/
+  environment.sessionVariables.XCURSOR_THEME = "Quintom_Snow";
+  environment.sessionVariables.XCURSOR_SIZE = 24;
+
+  environment.systemPackages = with pkgs; [
+    noctalia-shell # https://docs.noctalia.dev/getting-started/nixos/
+    nwg-displays # GUI multi-monitor layout editor
+    quintom-cursor-theme
+  ];
 }
