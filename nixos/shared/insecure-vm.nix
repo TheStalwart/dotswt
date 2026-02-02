afc53b298c87406874e35477576fe0bd5a6fad2f
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-02 19:45:13 +0200

    Figured out why ly ignored autoLogin settings

--- nixos/shared/insecure-vm.nix
+++ nixos/shared/insecure-vm.nix
@@ -9,9 +9,13 @@
 {
   security.sudo.wheelNeedsPassword = false;
 
-  # ly doesn't support auto login, use COSMIC Greeter instead
-  services.displayManager.ly.enable = lib.mkForce false;
-  services.displayManager.cosmic-greeter.enable = lib.mkForce true;
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "stalwart";
+
+  # ly ignores "services.displayManager.autoLogin" settings
+  # unless this parameter is set.
+  # I should probably report this to nixpkgs GitHub
+  services.displayManager.ly.settings = {
+    auto_login_service = "ly-autologin";
+  };
 }
