1b5bf216e4ad9c35939af3e4edaca68203afe7dd
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-02 23:36:09 +0200

    Added hyprprop to a list of systemPackages when running hyprland

--- nixos/desktops/hyprland.nix
+++ nixos/desktops/hyprland.nix
@@ -23,6 +23,7 @@
   environment.sessionVariables.XCURSOR_SIZE = 24;
 
   environment.systemPackages = with pkgs; [
+    hyprprop
     noctalia-shell # https://docs.noctalia.dev/getting-started/nixos/
     nwg-displays # GUI multi-monitor layout editor
     quintom-cursor-theme
