cad56b04de87bf2de7ab2636c40826e354f9cdf0
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-09 17:55:08 +0200

    Added a reminder to nixos-rebuild in screen or tty when running COSMIC

--- nixos/desktops/cosmic.nix
+++ nixos/desktops/cosmic.nix
@@ -1,5 +1,8 @@
 # Set preferred desktop environment to COSMIC
-
+#
+# COSMIC often crashes on nixos-rebuild,
+# so make sure to update the system in screen or tty.
+#
 # Include this file in /etc/nixos/configuration.nix imports section
 # and run `nixos-rebuild switch --upgrade-all` to sync system state
 
