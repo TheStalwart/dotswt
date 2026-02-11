f0cc720185578f0109bfae0dbe40181d67fb4ec3
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-11 22:04:43 +0200

    ly requires manual configuration of battery ID

--- nixos/hosts/gpd.nix
+++ nixos/hosts/gpd.nix
@@ -45,6 +45,10 @@ nixpkgs.lib.nixosSystem {
         };
       };
 
+      services.displayManager.ly.settings = {
+        battery_id = "max170xx_battery";
+      };
+
       # This value determines the NixOS release from which the default
       # settings for stateful data, like file locations and database versions
       # on your system were taken. It‘s perfectly fine and recommended to leave
