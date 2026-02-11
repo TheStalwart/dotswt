b906b6da9cc8e9b85f2d86bec74c0ea4271beaa5
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-11 22:57:54 +0200

    Added a link to documentation in ly.nix

--- nixos/displayManagers/ly.nix
+++ nixos/displayManagers/ly.nix
@@ -1,6 +1,8 @@
 {
   services.displayManager.ly = {
     enable = true;
+
+    # https://github.com/fairyglade/ly/blob/master/res/config.ini
     settings = {
       bigclock = "en";
       clear_password = true;
