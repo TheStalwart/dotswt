80db502653259eb9b2d228bc09ca3659cf3515df
Pavel Shevchuk <stlwrt@gmail.com>
2026-02-14 23:23:03 +0000

    Improved instructions for NixOS on Incus after deploying another instance

--- nixos/macon.nix
+++ nixos/macon.nix
@@ -1,11 +1,14 @@
 # NixOS Incus image is broken on TrueNAS out of box.
+# `nix-channel --update` will fail with the error:
+# "this system does not support the kernel namespaces that are required for sandboxing;"
+#
 # To fix it:
 # - Stop the container
 # - Open System -> Shell in TrueNAS Web Interface
 #   - `sudo -i`
 #   - `incus config set CONTAINERNAME security.nesting=true`
 # - Start the container
-# - Edit /etc/nix/nix.conf, set `nix.settings.sandbox = false;`
+# - Edit /etc/nixos/configuration.nix, set `nix.settings.sandbox = false;`
 # - `nix-channel --update`
 # - `nixos-rebuild switch --upgrade-all`
 # - Stop the container
@@ -13,7 +16,7 @@
 #   - `sudo -i`
 #   - `incus config unset CONTAINERNAME security.nesting`
 # - Start the container
-
+#
 # TrueNAS Incus dashboard opens "Container Shell" in a broken state.
 # Execute `su -` to evaluate all configuration files properly.
 
