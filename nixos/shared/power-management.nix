c526255001cef252617554355d443b1f0f1e49f0
Pavel Shevchuk <stlwrt@gmail.com>
2026-03-07 03:01:26 +0200

    Documented the problem with logind IdleAction settings

--- nixos/shared/power-management.nix
+++ nixos/shared/power-management.nix
@@ -2,6 +2,10 @@
   # Suspend if no activity on login screen.
   # After login, apps like hypridle will override these settings.
   #
+  # Or at least it's supposed to be like that,
+  # but as of March 2026, none of the Display Managers
+  # implement IdleHint correctly.
+  #
   # A notable exception is COSMIC,
   # which doesn't seem to inhibit logind defaults,
   # so it will suspend after 3 minutes even if user is logged in and active.
