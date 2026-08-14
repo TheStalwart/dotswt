008e0fbf3e6d9becdfae6793fd4a12d0751baa60
Pavel Shevchuk <stlwrt@gmail.com>
2026-08-14 15:45:16 +0300

    Enable natural_scroll for touchpads in hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -253,7 +253,7 @@ hl.config({
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
         touchpad = {
-            natural_scroll = false,
+            natural_scroll = true,
         },
     },
 })
