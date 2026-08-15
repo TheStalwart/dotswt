ea1ffb07bd11d820bbf1e5c880a295d13e1fdcc9
Pavel Shevchuk <stlwrt@gmail.com>
2026-08-15 18:53:53 +0300

    Set keyboard layouts in hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -253,10 +253,10 @@ hl.config({
 
 hl.config({
     input = {
-        kb_layout  = "us",
+        kb_layout  = "lv,ru",
         kb_variant = "",
         kb_model   = "",
-        kb_options = "",
+        kb_options = "grp:lalt_lshift_toggle",
         kb_rules   = "",
 
         follow_mouse = 1,
