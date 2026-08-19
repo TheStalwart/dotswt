30cc1f87511690b54923c3513bddf0b8e73873e7
Pavel Shevchuk <stlwrt@gmail.com>
2026-08-19 10:27:04 +0300

    Bound PrintScreen key to screen capture apps

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -362,6 +362,9 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+-- Screen capture
+hl.bind("Print", hl.dsp.exec_cmd("flatpak run --env=WAYSHOT_CAPTURE=1 io.github.gutopardini.wayshot || flatpak run org.flameshot.Flameshot gui"))
+
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
